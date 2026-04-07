014a7b41b5ebe001a254193d401f47e8daf1c304
Thiago Emanoel <thiagoemanoel448@gmail.com>
2026-04-07 15:21:35 -0300

    feat: :sparkles: implements counter

--- src/app/screens/counter/counter.ts
+++ src/app/screens/counter/counter.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Title } from '../../components/shared/title/title';
 
 @Component({
@@ -7,4 +7,19 @@ import { Title } from '../../components/shared/title/title';
   templateUrl: './counter.html',
   styleUrl: './counter.css',
 })
-export class Counter {}
+export class Counter {
+  // signal: forma de gerenciar estado reativo, estados locais
+  count = signal(0);
+
+  increment() {
+    this.count.update((n) => n + 1);
+  }
+
+  decrement() {
+    this.count.update((n) => n - 1);
+  }
+
+  reset() {
+    this.count.set(0);
+  }
+}
